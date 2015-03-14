7dfcb4a088f27eeefded312fc95c0277fa7c52d0
banal_a <axel.banal@epita.fr>
2015-03-14 15:34:50 -0700

    HomeFragment : Add Webview

--- app/src/main/java/com/csumb/dmdc/Fragment/HomeFragment.java
+++ app/src/main/java/com/csumb/dmdc/Fragment/HomeFragment.java
@@ -2,11 +2,15 @@ package com.csumb.dmdc.Fragment;
 
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 import com.csumb.dmdc.Activity.CropActivity;
@@ -28,15 +32,25 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         v =  inflater.inflate(R.layout.fragment_home, container, false);
-        Button button = (Button) v.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getActivity(), CropActivity.class);
-                getActivity().startActivity(i);
-            }
-        });
+        WebView myWebView = (WebView) v.findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new MyWebViewClient());
+        myWebView.loadUrl("http://www.dmdc.osd.mil/appj/dwp/index.jsp");
         return v;
+    }
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (Uri.parse(url).getHost().equals("www.example.com")) {
+                // This is my web site, so do not override; let my WebView load the page
+                return false;
+            }
+            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
     }
 
 
