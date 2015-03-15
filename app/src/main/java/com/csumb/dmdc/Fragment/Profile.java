066d8aaaa092d75bd83ef007ea80e4d48bf8faa0
banal_a <axel.banal@epita.fr>
2015-03-14 18:19:41 -0700

    Add : Profile

--- app/src/main/java/com/csumb/dmdc/Fragment/Profile.java
+++ app/src/main/java/com/csumb/dmdc/Fragment/Profile.java
@@ -9,7 +9,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.ParseException;
 import com.parse.ParseFile;
@@ -19,6 +21,7 @@ import com.parse.ParseUser;
 
 import com.csumb.dmdc.R;
 import com.parse.ParseUser;
+import com.squareup.picasso.Picasso;
 
 import java.lang.ref.WeakReference;
 import java.util.List;
@@ -40,6 +43,7 @@ public class Profile extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    ImageView img;
 
     List<ParseObject> ob;
     private List<Profile> Profile = null;
@@ -75,6 +79,7 @@ public class Profile extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
@@ -84,6 +89,12 @@ public class Profile extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_profile,container,false);
         startRemoteDataTask();
+        img = (ImageView) v.findViewById(R.id.imageView);
+        ParseFile file = ParseUser.getCurrentUser().getParseFile("profile_pic");
+        String str = ParseUser.getCurrentUser().getUsername();
+        Toast.makeText(getActivity(),str,Toast.LENGTH_LONG);
+        if(file!=null){
+        Picasso.with(getActivity()).load(file.getUrl()).into(img);}
         return v;
 
 
@@ -120,12 +131,16 @@ public class Profile extends Fragment {
                 ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("User");
 
                 query.orderByDescending("createdAt");
+                query.whereEqualTo("_User",ParseUser.getCurrentUser());
                 ob = query.find();
                 for(ParseObject info : ob)
                 {
-                    ParseFile username = (ParseFile) info.get("username");
+                    ParseFile username = (ParseFile) info.get("name");
                     ParseFile email = (ParseFile) info.get("email");
                     ParseFile profile_pic = (ParseFile) info.get("profile_pic");
+                    if(profile_pic!=null){
+                        Picasso.with(getActivity()).load(profile_pic.getUrl())
+                                .centerCrop().into(img);}
                 }
             }
             catch(ParseException e)
@@ -141,7 +156,7 @@ public class Profile extends Fragment {
             TextView user_text = (TextView) getView().findViewById(R.id.username);
             TextView email_text = (TextView) getView().findViewById(R.id.email);
             ParseUser current = ParseUser.getCurrentUser();
-            String username = current.getUsername();
+            String username = current.getString("name");
             String email = current.getEmail();
             user_text.setText(username);
             email_text.setText(email);
