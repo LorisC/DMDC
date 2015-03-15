76495be536ff5b1ab4363d9fab8eab463d1fdba5
banal_a <axel.banal@epita.fr>
2015-03-15 10:52:48 -0700

    Upload Profile

---
+++ app/src/main/java/com/csumb/dmdc/ParseClass/Mission.java
@@ -0,0 +1,14 @@
+package com.csumb.dmdc.ParseClass;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+/**
+ * Created by banal_a on 3/15/15.
+ */
+@ParseClassName("mission")
+public class Mission extends ParseObject {
+    public Mission(){
+
+    }
+}
